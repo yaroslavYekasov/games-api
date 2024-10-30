b4449b9f6f1d0080c951fe1a9be5eb13349f3f49
Yaroslav Yekasov <fngpr.56@gmail.com>
2024-10-30 02:19:53 +0200

    Initial commit [Delivers #188498464]

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const port = 8080;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:${port}/docs`);
+});
