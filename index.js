ae41c5956cf03d46a5a60199002ab5de9154f2a5
Yaroslav Yekasov <fngpr.56@gmail.com>
2024-10-30 02:37:16 +0200

    As an app developer i want to get a list of all games [Delivers #188498471]

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => {
   console.log(`API listening at http://localhost:${port}/docs`);
 });
+app.get('/games', (req, res) => {
+  res.send([
+    { name: 'Witcher 3' },
+    { name: 'Cyberpunk 2077' }
+  ]);
+});
